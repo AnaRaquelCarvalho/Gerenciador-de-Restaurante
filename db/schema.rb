742964039b669638f4492af4a4c891ec5df781be
Ana R. Carvalho <anarcarvalho93@gmail.com>
2025-12-17 11:12:37 -0300

    commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_16_013500) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_17_123537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,5 +24,12 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_16_013500) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 end
